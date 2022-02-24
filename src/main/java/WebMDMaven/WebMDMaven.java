d8f8622ea78806aa22ebfb8f01cb76d9f05072e8
Tasmia Chowdhury <48135500+TasmiaChowdhury@users.noreply.github.com>
2022-02-23 21:34:32 -0500

    WebMD maven URL setup Commited

--- src/main/java/WebMDMaven/WebMDMaven.java
+++ src/main/java/WebMDMaven/WebMDMaven.java
@@ -1,12 +1,28 @@
 package WebMDMaven;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
 public class WebMDMaven {
 
 	public static void main(String[] args) {
 		// this will be our Master Branch 
 		
-		System.out.println("This is our Master Branch ");
-
+		//System.out.println("This is our Master Branch ");
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver(); 
+		
+		
+		driver.get("https://www.webmd.com/"); 
+		driver.manage().window().maximize(); 
+		
+		
 	}
 
 }
