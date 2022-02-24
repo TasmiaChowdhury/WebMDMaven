f4aa5b7b32ac5fcebadfeac95988a9600fa80a5b
Tasmia Chowdhury <48135500+TasmiaChowdhury@users.noreply.github.com>
2022-02-23 20:23:02 -0500

    First Commit

---
+++ src/main/java/WebMDMaven/WebMDMaven.java
@@ -0,0 +1,12 @@
+package WebMDMaven;
+
+public class WebMDMaven {
+
+	public static void main(String[] args) {
+		// this will be our Master Branch 
+		
+		System.out.println("This is our Master Branch ");
+
+	}
+
+}
